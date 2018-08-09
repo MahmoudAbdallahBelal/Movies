f4b65e405949bef34e134141864e267ab7c12f3e
mahmoudbelalcs@gmail.com <mahmoudbelal@FCI012>
2018-08-10 01:56:51 +0200

    add new fragment

---
+++ app/src/main/java/com/example/belal/movies/presentation/movies_list/display_movie_details/MovieDetailsFragment.java
@@ -0,0 +1,30 @@
+package com.example.belal.movies.presentation.movies_list.display_movie_details;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.belal.movies.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MovieDetailsFragment extends Fragment {
+
+
+    public MovieDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_details, container, false);
+    }
+
+}
